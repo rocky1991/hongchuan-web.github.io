c5317209ee5abe6c58a38a0148bac79590316958
rocky1991 <hongchuan91@gmail.com>
2019-05-27 21:53:47 -0600

    finished Strings

--- javascript/myS.js
+++ javascript/myS.js
@@ -2,5 +2,6 @@ function eFunc(){
 	var a = {firstName:"John",
 			 lastName:"Doe"};
 	var cars = ["Saab","bmw","Benz"]
-	document.getElementById("demo1").innerHTML= typeof cars;
+	// document.getElementById("demo1").innerHTML= typeof cars;
+	document.getElementById("demo1").innerHTML= Date();
 }
