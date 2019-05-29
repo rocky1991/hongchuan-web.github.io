c0b7a3aec626cda55ee1f1042f41126143067352
rocky1991 <hongchuan91@gmail.com>
2019-05-29 16:30:00 -0600

    finished String methods

--- javascript/myS.js
+++ javascript/myS.js
@@ -2,6 +2,8 @@ function eFunc(){
 	var a = {firstName:"John",
 			 lastName:"Doe"};
 	var cars = ["Saab","bmw","Benz"]
+	var str = "mystrimghahaimg"
 	// document.getElementById("demo1").innerHTML= typeof cars;
-	document.getElementById("demo1").innerHTML= Date();
+	// document.getElementById("demo1").innerHTML= Date();
+	document.getElementById("demo1").innerHTML= str.search("ing");
 }
