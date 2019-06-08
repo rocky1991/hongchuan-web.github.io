3441a60ff37075252b2c110884e557b2c106ce29
rocky1991 <hongchuan91@gmail.com>
2019-06-08 11:19:32 -0600

    finished Array Sort

--- javascript/myS.js
+++ javascript/myS.js
@@ -3,9 +3,25 @@ function eFunc(){
 			 lastName:"Doe"};
 	var cars = ["Saab","bmw","Benz"]
 	var str = "mystrimghahaimg"
+	var nums = [40,100,1,5,23]
+	cars.sort()
+	cars.reverse()
 	// document.getElementById("demo1").innerHTML= typeof cars;
 	// document.getElementById("demo1").innerHTML= Date();
 	// document.getElementById("demo1").innerHTML= str.search("ing");
-	document.getElementById("demo1").innerHTML = cars.toString();
+	// document.getElementById("demo1").innerHTML = cars.toString();
+	// document.getElementById("demo1").innerHTML = nums.sort(function(a,b){return a-b});
+	document.getElementById("demo1").innerHTML = findArrMax(nums);
 	
+}
+function findArrMax(arr){
+
+	var cur_max = -Infinity;
+	var i;
+	for(i=0;i<arr.length;i++){
+		if(arr[i]>cur_max){
+			cur_max = arr[i];
+		}
+	}
+	return cur_max;
 }
