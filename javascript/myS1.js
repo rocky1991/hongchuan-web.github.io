947c1374208f4146cb9673f6a2f8fcd4d91f6389
rocky1991 <hongchuan91@gmail.com>
2019-06-09 20:43:31 -0600

    finished booleans

--- javascript/myS1.js
+++ javascript/myS1.js
@@ -1,5 +1,7 @@
 function myF(){
 	var d = new Date(2019,10,6,12,23,5,23)
 	var d1 = new Date("2015-11-02")
-	document.getElementById("demo1").innerHTML = d1.getDate();
+	var arr = [5,3,2,6]
+	// document.getElementById("demo1").innerHTML = d1.getDate();
+	document.getElementById("demo1").innerHTML= Math.max(5,3,2,6);
 }
