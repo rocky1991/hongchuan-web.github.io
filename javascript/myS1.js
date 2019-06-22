176c5dcd83699c3f7d903cdb4dc789685c8af3bd
rocky1991 <hongchuan91@gmail.com>
2019-06-21 22:21:39 -0600

    finished Function definitions

--- javascript/myS1.js
+++ javascript/myS1.js
@@ -3,6 +3,12 @@ function myF(){
 	var d1 = new Date("2015-11-02")
 	var arr = [5,3,2,6]
 	// document.getElementById("demo1").innerHTML = d1.getDate();
-	document.getElementById("demo1").innerHTML= Math.max(5,3,2,6);
+	// document.getElementById("demo1").innerHTML= Math.max(5,3,2,6);
+	var add = function(a,b){return a + b;};
+	const sub = function(a,b){return a-b;};
+	const arrow = (a,b) => {return a * b};
+	// document.getElementById("demo1").innerHTML = add(3,5);S
+	// document.getElementById("demo1").innerHTML = sub(5,2);
+	document.getElementById("demo1").innerHTML = arrow(3,2);
 	
 }
