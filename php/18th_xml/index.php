b04619c7fc2a44f332f303732f0c079581bf8c3e
rocky1991 <hongchuan91@gmail.com>
2019-07-14 16:17:40 -0600

    worked on XML expat

--- php/18th_xml/index.php
+++ php/18th_xml/index.php
@@ -4,6 +4,25 @@
 </head>
 <body>
 	<?php
+	$xml3 = simplexml_load_file("books.xml") or die("Error: cannot create object");
+	echo $xml3->book[0]->title . "<br>";
+	echo $xml3->book[1]->title . "<br>";
+	echo $xml3->book[0]['category']."<br>";
+	foreach($xml3->children() as $books) {
+		echo $books->title .",";
+		echo $books->author .",";
+		echo $books->year . ",";
+		echo $books->price. ",";
+	}
+
+
+
+	$xml2=simplexml_load_file("note.xml") or die("Error: cannot create object");
+	echo $xml2->to ."<br>";
+	echo $xml2->heading ."<br>";
+
+
+
 	libxml_use_internal_errors(true);
 	$myXMLData = 
 		"<?xml version='1.0' encoding='UTF-8'?>
