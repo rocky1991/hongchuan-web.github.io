72716295dd95336c396d93232a2d810225b4484d
rocky1991 <hongchuan91@gmail.com>
2019-07-17 09:59:43 -0600

    stopped at C++ variables

---
+++ c++/README.md
@@ -0,0 +1,2 @@
+sudo apt install g++
+g++ -o learn learn.cpp && ./learn
---
+++ c++/learn.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// this is comment
+	cout << "hello world!\n" << endl;
+	cout << "another hello" << endl;
+	/*
+	* this is block comment
+	*/
+	int a = 15;
+	cout << a << endl;
+	const int a1 = 10;
+	cout << a1 << endl;
+	int age1 = 234;
+	cout << "my age is " << age1 << " years old\n";
+	return 0;
+}
