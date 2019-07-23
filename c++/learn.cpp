ad6825caf11f38bdfbf69a6cc5070e8651518b8d
rocky1991 <hongchuan91@gmail.com>
2019-07-23 12:18:43 -0600

    finished c++

--- c++/learn.cpp
+++ c++/learn.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main() {
+	string food = "Pizza";
+	string &meal = food;
+	cout << &food << "\n";
+	cout << &meal << "\n";
+	string * pt = &food;
+	cout << "pointer value: " << pt << "\n";
+	cout << "pointer: " << *pt << "\n";
+	cout << meal << "\n";
+	*pt = "ham";
+	cout << "now the food is: " << food << "\n";
+
+	string students[3];
+	string cars[3] = {"an","en","bn"};
+
+	bool isFun = true;
+	cout << isFun << endl;
+
+	cout << (5>3) << endl;
+	cout << max(5,10) << endl;  // not working for more than 2 numbers;
+	cout << sqrt(64) << endl;
+	cout << log(2) << endl;
+	int a = 5;
+
+	if (a>3 ) {
+		cout << "condition 1" << endl;
+	} else if (a <5) {
+		cout << "condition 2" << endl;
+	} else {
+		cout << "last condition" << endl;
+	}
+	string result = (a <6) ? "haha" : "no haha";
+	cout << result << endl;
+
+	switch(a) {
+		case 5:
+		cout << "a five";
+		break;
+		default:
+		cout << "not a five";
+	}
+	cout <<"\n";
+	/*
+
 	string input;
 	string input1;
 	// cout << "Type full name:";
@@ -32,15 +77,19 @@ int main() {
 
 	// this is comment
 	cout << "hello world!\n" << endl;
-	cout << "another hello" << endl;
+	cout << "another hello" << endl; */
 	/*
 	* this is block comment
 	*/
+
+	/*
 	int a = 15;
 	cout << a << endl;
 	const int a1 = 10;
 	cout << a1 << endl;
 	int age1 = 234;
-	cout << "my age is " << age1 << " years old\n";
+	cout << "my age is " << age1 << " years old\n"; */
+
 	return 0;
+
 }
