5a018924840b03c09327e3fd59883948ce2a6bae
rocky1991 <hongchuan91@gmail.com>
2019-08-30 09:35:12 -0600

    get to annotating text

--- python-plot/usage_guide/matplotlib_learn.py
+++ python-plot/usage_guide/matplotlib_learn.py
@@ -2,6 +2,110 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import pandas
 
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000)
+
+n, bins, patches = plt.hist(x, 50, density=1, facecolor='g', alpha=0.75)
+
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+plt.show()
+
+
+
+
+
+'''
+plt.figure(1)
+plt.subplot(211)
+plt.plot([1, 2, 3])
+plt.subplot(2,1,2)
+plt.plot([4,5,6])
+
+plt.figure(2)
+plt.plot([4,5,6])
+
+plt.figure(1)
+plt.subplot(2,1,1)
+plt.title("Easy as 1, 2, 3")
+
+plt.show()
+'''
+
+
+
+'''
+def f(t):
+	return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0., 5., 0.1)
+t2 = np.arange(0., 5., 0.02)
+
+plt.figure()
+plt.subplot(211)
+plt.plot(t1, f(t1), 'bo', t2, f(t2), 'r-')
+
+plt.subplot(2,1,2)
+plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+plt.show()
+'''
+
+
+'''
+x = np.arange(0,50)
+lines = plt.plot(x, x)
+print(plt.setp(lines))
+'''
+
+'''
+names = ['group_a', 'group_b', 'group_c']
+values = [1, 10, 100]
+
+plt.figure(figsize=(9, 3))
+
+plt.subplot(131)
+plt.bar(names, values)
+plt.subplot(132)
+plt.scatter(names, values)
+plt.subplot(133)
+plt.plot(names, values)
+plt.suptitle('Categorical Plotting')
+plt.show()
+'''
+
+'''
+data = {'a': np.arange(50),
+		'c': np.random.randint(0, 50, 50),
+		'd': np.random.randn(50)}
+
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d']) * 100
+
+plt.scatter('c', 'd', c='c', s='d', data=data)
+plt.xlabel('entry a')
+plt.ylabel('entry b')
+plt.show()
+'''
+
+'''
+t = np.arange(0., 5., 0.2)
+
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+plt.show()
+'''
+
+'''
+plt.plot([1, 2, 3, 4], [2, 3, 4, 5], 'ro')
+plt.ylabel('some numbers')
+plt.axis([0, 6, 0, 10])   # this specifies the viewport of the axes
+plt.show()
+'''
+
+
 '''
 def my_plotter(ax, data1, data2, param_dict):
 	"""
