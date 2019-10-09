8db692f9eca47ff7a619db0e0c76fad57f55533a
rocky1991 <hongchuan91@gmail.com>
2019-10-09 10:50:52 -0600

    minor update

---
+++ topcoder/ABCPath.java
@@ -0,0 +1,14 @@
+public class ABCPath {
+
+	public int length(String [] grid) {
+        int len = 5;
+        System.out.println("Length is " + len);
+        return len;
+    }
+
+    public static void main(String [] args) {
+
+    	
+    	
+    }
+}
