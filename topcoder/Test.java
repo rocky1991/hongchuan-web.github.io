8e09d27634af351bf14ed1be807ff1195b8b9b92
rocky1991 <hongchuan91@gmail.com>
2019-10-29 13:57:35 -0600

    solved ABCPath problem

---
+++ topcoder/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+	public static void main(String [] args) {
+		String myStr = "abc";
+		char a = 'a';
+		// char b = 'b';
+		System.out.println(myStr.charAt(myStr.length() -1));
+		System.out.println((int)a +1);
+		char b = (char)((int)a + 1);
+		System.out.println(b);
+		String test = "abd" + b;
+		System.out.println(test);
+		boolean c = true;
+		System.out.println(c);
+	}
+}
