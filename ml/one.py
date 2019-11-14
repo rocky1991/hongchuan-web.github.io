a9f8731873fdf10d22a360bb827256718340f9ed
rocky1991 <hongchuan91@gmail.com>
2019-11-14 12:39:23 -0700

    add data set to ml

---
+++ ml/one.py
@@ -0,0 +1,8 @@
+import numpy as np 
+
+# rows of data set is called the size of data
+# columns of data set is called the dimension of data
+# the analysis of single attribute is refered t oas univariage analysis
+# the simultaneous analysis of two attribute is called bivariate analysis
+# the simultaneous analysis of multiple attribute is called multivariate analysis
+
