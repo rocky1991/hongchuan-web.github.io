04c61d0438639425149253683e34c1691106704d
rocky1991 <hongchuan91@gmail.com>
2019-12-03 12:24:04 -0700

    minor update

--- data_structures_java/1.2/Notebook.java
+++ data_structures_java/1.2/Notebook.java
@@ -2,6 +2,7 @@ public class Notebook extends Computer {
 	// Data fields
 	private double screenSize;
 	private double weight;
+	private static final String DEFAULT_NB_MAN = "MyBrand";
 
 	// Methods 
 	/** Initializes a Notebook object with all properties specified.
@@ -18,10 +19,19 @@ public class Notebook extends Computer {
 		screenSize = screen;
 		weight = wei;
 	}
+
+	public Notebook(String proc, double ram, int disk, double procSpeed, double screen, double wei){
+		this(DEFAULT_NB_MAN, proc, ram, disk, procSpeed, screen, wei);
+	}
 	public double getScreen() { return screenSize; }
 	public double getWei() { return weight; }
 
 	public void setScreen(double scrSize) { screenSize = scrSize; }
 	public void setWei(double wei) { weight = wei; }
 	
-}
+	@Override
+	public String toString(){
+		String result = super.toString() + "Screen Size: " + screenSize + "\nWeight: " + weight + "\n";
+		return result;
+	}
+} 	
