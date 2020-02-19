8ce5b8de98bb422f1b901523b9397f0c7bfa0741
rocky1991 <hongchuan91@gmail.com>
2020-02-19 11:22:15 -0700

    update fortran

--- fortran/hello.f
+++ fortran/hello.f
@@ -5,6 +5,8 @@
 
 		write(*,*) test_int
 		write(*,*) test_int2
-		write(*,*) "test message"
-	
+		write(*,"(i2,A)",advance='NO') 5, "test"
+		write(*,'(i1)',advance='NO') 9
+		
+		write(*,*) "I am"//"a student"
 		end program greedyAlgo
