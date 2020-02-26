1ad89072d338bbfd26e4a55dd9971c39e4de2ea8
rocky1991 <hongchuan91@gmail.com>
2020-02-26 16:37:28 -0700

    go finished packaged

--- Go/hello.go
+++ Go/hello.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 import "io/ioutil"
 import "os"
+import "time"
+import "math/rand"
 
 func main() {
 	data, err := ioutil.ReadFile(os.Args[1])
@@ -11,4 +13,9 @@ func main() {
 	}
 	fmt.Println(string(data))
 
+	fmt.Println("The time is", time.Now())
+
+	// environment in which the program is executed is deterministic, so each time rand is called, same number is returned. 
+	fmt.Println(rand.Intn(100))
+	fmt.Println(rand.Intn(100))
 }
