3e3f1d05a4bd83e7ed420dd666dba216a6c5f8a2
rockyhw <hongchuan91@gmail.com>
2021-03-15 23:39:46 -0600

    get to K&R 2.4

--- doc/C/K_R/chap2/exer2_1.c
+++ doc/C/K_R/chap2/exer2_1.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-
+#include <string.h>
 int main() {
 	long a = 12345l;
 	unsigned char b = 256;
 	printf("%d\n", b);
+	char c = '\013';
+	printf("%c\n", c);
+	char d = '\a';
+	printf("%c %c %c %c %c \n", d, '\\', '\?', '\'', '\"' );
+	char la[3] = "ab";
+	printf("the string is %s\n", la);
+	printf("the string length is %ld\n", strlen(la));
 	return 0;
+
 }
