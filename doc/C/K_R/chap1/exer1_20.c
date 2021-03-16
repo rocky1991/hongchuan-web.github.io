b9390748219eef97aa2f279e792baff8eb486bcc
rockyhw <hongchuan91@gmail.com>
2021-03-16 12:11:56 -0600

    skipped end of chap exersises for chap1

---
+++ doc/C/K_R/chap1/exer1_20.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int my_getline(char line[], int limit);
+int main() {
+	return 0;
+}
+
+int my_getline(char line[], int limit) {
+	int c, i;
+	for (i = 0; i < limit -1 && ((c = getchar()) != EOF && c != '\n'; ++i) 
+		s[i] = c;
+	if (c == '\n')
+		s[i] = c;
+		++i;
+	s[i] = '\0';
+
+	return 0;
+}
+	
