b02c1f60023b42c3c05a80491324d1d8602aa202
rockyhw <hongchuan91@gmail.com>
2021-03-16 12:45:30 -0600

    find range using overflow for exer2_1

--- doc/C/K_R/chap2/exer2_1.c
+++ doc/C/K_R/chap2/exer2_1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
 	long a = 12345l;
 	unsigned char b = 256;
@@ -11,6 +12,34 @@ int main() {
 	char la[3] = "ab";
 	printf("the string is %s\n", la);
 	printf("the string length is %ld\n", strlen(la));
+
+	signed char sc = 0;
+	// check upper bound
+	signed char prev_sc = 0;
+	while (1) {
+		sc += 1;
+		if (sc < prev_sc)
+			break;
+		else 
+			prev_sc = sc;
+	}
+	printf("biggest sc is %d\n", prev_sc);
+	while (1) {
+		sc -= 1;
+		if (sc > prev_sc) 
+			break;
+		else 
+			prev_sc = sc;
+	}
+	printf("The smallest sc is %d\n", prev_sc);
+	signed short ss = 0;
+	signed int si = 0;
+	signed long sl= 0l;
+	unsigned char uc= 0;
+	unsigned short us= 0;
+	unsigned int ui= 0;
+	unsigned long ul= 0ul;
+	
 	return 0;
 
 }
